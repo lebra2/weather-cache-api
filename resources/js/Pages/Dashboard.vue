ea9d0876e56f476199b23c2e1d34346b7c5d2374
Markus <lebrabusiness@gmail.com>
2024-04-17 14:56:10 +0300

    Demo styling for weather show

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,16 +21,18 @@ console.log(props.data)
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <h1>Weather app</h1>
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg">
+                    <h1 class="text-2xl">Weather app</h1>
                 </div>
             </div>
-            <div class="flex flex-col justify-center items-center">
-                <h2>{{ props.data.name }}</h2>
-                <img :src="`https://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`" alt="">
-                <h2>Kraadi {{ props.data.main.temp }}</h2>
-                <h2>Tundub nagu {{ props.data.main.feels_like }}</h2>
-                <h2>{{ props.data.name }}</h2>
+            <div class="flex flex-col justify-center items-center mt-12 ">
+                <div class="flex flex-col items-center bg-slate-300 rounded-lg p-16">
+                    <h2 class="text-2xl">{{ props.data.name }}</h2>
+                    <img class="w-24 h-24" :src="`https://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`" alt="">
+                    <h2 class=text-xl>{{ props.data.main.temp }} °</h2>
+                    <h2 class="text-xl">Tundub nagu {{ props.data.main.feels_like }}</h2>
+                </div>
+
             </div>
 
         </div>
