c122083518257d750c858f45eada00e7dd99300f
Markus <lebrabusiness@gmail.com>
2024-04-17 14:50:50 +0300

    Caching

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -15,7 +16,14 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    Cache::remember('weather', now()->addHour(), fn () => Http::get('https://api.openweathermap.org/data/2.5/weather', [
+        'q' => 'Kuressaare',
+        'appId' => 'bd23c556416f3e51bc349fdc92b7399e',
+        'units' => 'metric'
+    ])->json());
+    return Inertia::render('Dashboard', [
+        'data' => Cache::get('weather')
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
