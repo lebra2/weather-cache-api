e121f85893080eebd481636e7b80cc12723c2462
Markus <lebrabusiness@gmail.com>
2024-04-17 14:51:04 +0300

    Show degrees on Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,15 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+const props = defineProps({
+    data: Object
+})
+
+console.log(props.data)
+
 </script>
+
 
 <template>
     <Head title="Dashboard" />
@@ -14,9 +22,17 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <h1>Weather app</h1>
                 </div>
             </div>
+            <div class="flex flex-col justify-center items-center">
+                <h2>{{ props.data.name }}</h2>
+                <img :src="`https://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`" alt="">
+                <h2>Kraadi {{ props.data.main.temp }}</h2>
+                <h2>Tundub nagu {{ props.data.main.feels_like }}</h2>
+                <h2>{{ props.data.name }}</h2>
+            </div>
+
         </div>
     </AuthenticatedLayout>
 </template>
